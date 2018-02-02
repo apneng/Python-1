de0827fad858713bfc12999e40e13711e6e6b89b
apneng <903060@163.com>
2018-02-02 15:10:05 +0800

    更新biying

--- biyingSpider.py
+++ biyingSpider.py
@@ -5,10 +5,11 @@ local = time.strftime("%Y.%m.%d")
 url = 'http://cn.bing.com/'
 con = requests.get(url)
 content = con.text
-reg = r"(http://s.cn.bing.net/az/hprichbg/rb/.*?.jpg)"
+# reg = r"(http://s.cn.bing.com/az/hprichbg/rb/.*?.jpg)"
+reg = r"(/az/hprichbg/rb/.*?.jpg)"
 a = re.findall(reg, content, re.S)[0]
-print(a)
-read = requests.get(a)
+print("https://cn.bing.com"+a)
+read = requests.get("https://cn.bing.com"+a)
 f = open('%s.jpg' % local, 'wb')
 f.write(read.content)
 f.close()
